eb3cff5792f109ea343dc353805e9c6bd969b906
AshwinBarnwal <therealdynatime@gmail.com>
2024-05-08 16:05:28 +0530

    working switch

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toggle";
 import { useTheme } from "next-themes";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ModeSwitch } from "@/components/mode-switch";
 
 export default function Home() {
   return (
@@ -8,7 +10,7 @@ export default function Home() {
   <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="flex space-x-4">
           <Button>Hello</Button>
-          <ModeToggle />
+          <ModeSwitch />
         </div>
   </div>
 </div>
