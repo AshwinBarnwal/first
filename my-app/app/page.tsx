61080088ce51ab83a3ae4a6d88c4044c76b6db91
AshwinBarnwal <therealdynatime@gmail.com>
2024-05-08 07:15:20 +0530

    theme toggle

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
           <ul className="space-y-4">
             <li className="flex items-center">
-              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
@@ -23,7 +24,7 @@ export default function Home() {
               </p>
             </li>
             <li className="flex items-center">
-              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
@@ -33,14 +34,16 @@ export default function Home() {
               </p>
             </li>
             <li className="flex items-center">
-              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+              <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
               <p className="ml-4">Code completion with instant preview</p>
             </li>
           </ul>
-          <Button variant="default" className="bg-green-500 hover:bg-green-700">ello</Button>
+          <Button variant="default" className="bg-green-500 hover:bg-green-700 mr-4">Hello</Button>
+          <p />
+          <ModeToggle />
           <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
         </div>
         <div className="pt-8 text-base font-semibold leading-7">
